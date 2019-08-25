8780c50a206aca3a901366480beacf92da8ea439
lesliearkorful <>
2019-08-25 23:56:11 +0000

    Update home-widgets.dart

--- showcase_web/lib/blog/screens/home/home-widgets.dart
+++ showcase_web/lib/blog/screens/home/home-widgets.dart
@@ -1,11 +1,13 @@
-//import 'package:flutter_web_ui/ui.dart' as ui;
 import 'package:flutter_web/material.dart';
-import '../../data/constants.dart';
 import '../../data/articles.dart';
+import '../../screens/article/article.dart';
 
 
 
 class Header extends StatelessWidget {
+  static final DateTime today = DateTime.now();
+  final String time = '${months[today.month]} ${today.day}'.toUpperCase();
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -15,14 +17,16 @@ class Header extends StatelessWidget {
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text('Sunday 4 August',
+              Text(time,
                 style: TextStyle(
                   color: Colors.pink,
                   fontSize: 16,
                 )
               ),
 
-              Text('Blog',
+              SizedBox( height: 5 ),
+
+              Text('Today',
                 style: TextStyle(
                   fontSize: 30,
                   fontWeight: FontWeight.bold,
@@ -35,7 +39,8 @@ class Header extends StatelessWidget {
             child: Container(),
           ),
 
-          ClipOval(
+          ClipRRect(
+            borderRadius: BorderRadius.circular(14),
             child: Image.asset('$avatars/les.jpg', width: 50 ),
           ),
         ],
@@ -60,17 +65,11 @@ class Carousel extends StatelessWidget {
       child: PageView.builder(
         controller: pageController,
         scrollDirection: Axis.horizontal,
-        itemCount: 4,
+        itemCount: 3,
         itemBuilder: (context, index) {
           Article data = articles[index];
 
-          return Card(
-            author: data.author,
-            authorImage: data.authorImage,
-            cover: data.cover,
-            time: data.time,
-            title: data.title,
-          );
+          return Card(article: data);
         },
       ),
     );
@@ -81,118 +80,119 @@ class Carousel extends StatelessWidget {
 
 
 class Card extends StatelessWidget {
-  final String cover;
-  final String author;
-  final String authorImage;
-  final String title;
-  final String time;
+  final Article article;
 
   Card({
-    @required this.title,
-    @required this.time,
-    @required this.author,
-    @required this.authorImage,
-    @required this.cover,
+    @required this.article,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(6),
-      width: MediaQuery.of(context).size.width ,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        image: DecorationImage(
-          image: AssetImage(cover),
-          fit: BoxFit.cover
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(
+            builder: (context) => ArticlePage(article: article)
+          )
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.all(6),
+        width: MediaQuery.of(context).size.width ,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          image: DecorationImage(
+            image: AssetImage(article.cover),
+            fit: BoxFit.cover
+          ),
         ),
-      ),
-      child: ClipRRect(
-      borderRadius: BorderRadius.circular(16),
-      child: Stack(
-          fit: StackFit.loose,
-          children: <Widget>[
+        child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: Stack(
+            fit: StackFit.loose,
+            children: <Widget>[
 
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: Container(
-                height: 250,
-                width: double.maxFinite,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Colors.transparent,
-                      Colors.black38
-                    ]
-                  )
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Container(
+                  height: 250,
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.transparent,
+                        Colors.black45
+                      ]
+                    )
+                  ),
                 ),
               ),
-            ),
 
-            Container(
-              padding: EdgeInsets.all(30),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
+              Container(
+                padding: EdgeInsets.fromLTRB(30, 20, 30, 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
 
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: Icon(Icons.label, color: Colors.white),
-                  ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Image.asset('$icons/white/017-bookmark.png')
+                    ),
 
-                  Column(
+                    Column(
                       mainAxisSize: MainAxisSize.min,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text('$title',
+                        Text('${article.title}',
                           style: TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.w500,
                             fontFamily: 'Montserrat',
-                            fontSize: 30,
-                            
+                            fontSize: 28,
                           ),
                         ),
-                        SizedBox( height: 20 ),
+                        SizedBox( height: 15 ),
                         Row(
                           children: <Widget>[
-                            ClipOval(
-                              child: Image.asset('$authorImage',
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(14),
+                              child: Image.asset('${article.authorImage}',
                                 width: 44,
                                 height: 44,
                               )
                             ),
-                            SizedBox( width: 15 ),
+                            SizedBox( width: 12 ),
                             Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
-                                Text('$author',
+                                Text('${article.author}',
                                   style: TextStyle(
                                     color: Colors.white,
                                     fontSize: 18,
-                                    fontWeight: FontWeight.bold
+                                    fontWeight: FontWeight.w500,
+                                    letterSpacing: 0.4,
                                   ),
                                 ),
                                 SizedBox( height: 5),
-                                Text('$time',
+                                Text('${article.time}',
                                   style: TextStyle(
-                                    color: Colors.white,
+                                    color: Colors.white.withOpacity(0.85),
                                     fontSize: 14
-                                  ),
-                                ),
-                              ],
+                                  )
+                                )
+                              ]
                             )
-                          ],
+                          ]
                         )
-                      ],
-                    ),
-                ]
+                      ]
+                    )
+                  ]
+                )
               )
-            )
-          ],
-        ),
+            ]
+          )
+        )
       )
     );
   }
@@ -213,13 +213,13 @@ class SectionHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.fromLTRB(homePadding, 30, homePadding, 10),
+      margin: EdgeInsets.fromLTRB(homePadding, 30, homePadding, 15),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Text('$title',
             style: TextStyle(
-              fontSize: 30,
+              fontSize: 28,
               fontWeight: FontWeight.w500
             ),
           ),
@@ -239,55 +239,61 @@ class SectionHeader extends StatelessWidget {
 
 
 class SectionItem extends StatelessWidget {
-  final String image;
-  final String title;
-  final Category category;
+  final Article article;
 
   SectionItem({
-    @required this.image,
-    @required this.title,
-    @required this.category,
+    @required this.article,
   });
 
   @override
   Widget build(BuildContext context) {
 
     return FlatButton(
-      onPressed: () {},
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(
+            builder: (context) => ArticlePage(article: article)
+          )
+        );
+      },
       splashColor: Colors.pink.withOpacity(0.15),
       highlightColor: Colors.pink.withOpacity(0.08),
       child: Container(
-        margin: EdgeInsets.fromLTRB(10, 20, homePadding, 10),
+        margin: EdgeInsets.fromLTRB(10, 15, homePadding, 15),
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             ClipRRect(
               borderRadius: BorderRadius.circular(14),
-              child: Image.asset(image,
+              child: Image.asset(article.cover,
                 width: 60,
                 height: 60,
                 fit: BoxFit.cover,
               )
             ),
             SizedBox( width: 15 ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('${categories[category]}',
-                  style: TextStyle(
-                    color: Colors.pink,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                textDirection: TextDirection.ltr,
+                children: <Widget>[
+                  Text('${categories[article.category]}',
+                    style: TextStyle(
+                      color: Colors.pink,
+                      fontSize: 14,
+                    ),
                   ),
-                ),
-                SizedBox( height : 5 ),
-                Text('$title',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20
+                  SizedBox( height : 5 ),
+                  Text('${article.title}',
+                  softWrap: true,
+                    style: TextStyle(
+                      //color: Colors.black.withOpacity(0.7),
+                      fontSize: 18
+                      //fontWeight: FontWeight.bold
+                    )
                   ),
-                ),
-              ]
-            )
+                ]
+              )
+            ),
           ]
         ),
       )
