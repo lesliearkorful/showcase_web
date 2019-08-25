047b5bacc5ff73287ab6aa2a6cfdef9063e0a6f6
lesliearkorful <>
2019-08-25 23:55:34 +0000

    Update constants.dart

--- showcase_web/lib/blog/data/constants.dart
+++ showcase_web/lib/blog/data/constants.dart
@@ -1,4 +1,22 @@
 String chalewote = 'images/chalewote';
 String deon = 'images/deon';
 String avatars = 'images/avatars';
+String flutter = 'images/flutter';
+String icons = 'icons/';
 double homePadding = 30.0;
+
+
+final Map<int, String> months = {
+  1 : 'January',
+  2 : 'February',
+  3 : 'March',
+  4 : 'April',
+  5 : 'May',
+  6 : 'June',
+  7 : 'July',
+  8 : 'August',
+  9 : 'September',
+  10 : 'October',
+  11: 'November',
+  12 : 'December',
+};
