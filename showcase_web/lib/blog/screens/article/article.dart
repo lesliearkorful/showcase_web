7b1e1242554c4959cf05888beb322af218e7c963
lesliearkorful <>
2019-08-26 13:15:26 +0000

    Update article.dart

--- showcase_web/lib/blog/screens/article/article.dart
+++ showcase_web/lib/blog/screens/article/article.dart
@@ -3,29 +3,68 @@ import '../../data/articles.dart';
 
 
 
-class ArticlePage extends StatelessWidget {
+class ArticlePage extends StatefulWidget {
   final Article article;
   ArticlePage({ this.article });
 
+  @override
+  _ArticlePageState createState() => _ArticlePageState();
+}
+
+class _ArticlePageState extends State<ArticlePage> {
+  final ScrollController listController = ScrollController();
+
+  bool topReached = false;
+  bool isSaved = false;
+
   final Image saveIcon = Image.asset('$icons/white/017-bookmark.png');
+  final Image saveIconPink = Image.asset('$icons/pink/017-bookmark.png');
+
+  final Image savedIcon = Image.asset('$icons/white/018-bookmarked.png');
+  final Image savedIconPink = Image.asset('$icons/pink/018-bookmarked.png');
+
   final Image backIcon = Image.asset('$icons/white/001-left-chevron.png');
+  final Image backIconPink = Image.asset('$icons/pink/001-left-chevron.png');
 
-  
 
+  @override
+  void initState() {
+    listController.addListener(() {
+      
+      setState(() {
+        topReached = (listController.offset > 225)
+                      ? true
+                      : false;
+      });
+    });
+    super.initState();
+  }
+
+
+  @override
+  void dispose() {
+    listController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Stack(
+      body: Center(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxWidth: 450
+          ),
+          child: Stack(
             children: <Widget>[
               
               Container(
                 height: (MediaQuery.of(context).size.height / 2) + 30,
                 decoration: BoxDecoration(
                   image: DecorationImage(
-                    image: AssetImage(article.cover),
+                    image: AssetImage(widget.article.cover),
                     fit: BoxFit.cover
                   ), 
                 ),
@@ -35,7 +74,8 @@ class ArticlePage extends StatelessWidget {
               Align(
                 alignment: Alignment.topCenter,
                 child: ListView(
-                  semanticChildCount: article.gallery.length,
+                  controller: listController,
+                  semanticChildCount: widget.article.gallery.length,
                   children: <Widget>[
 
                     SizedBox( height: (MediaQuery.of(context).size.height/2) - 30 ),
@@ -59,7 +99,7 @@ class ArticlePage extends StatelessWidget {
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Text(categories[article.category],
+                          Text(categories[widget.article.category],
                             style: TextStyle(
                               color: Colors.pink,
                               fontSize: 16,
@@ -68,7 +108,7 @@ class ArticlePage extends StatelessWidget {
 
                           SizedBox( height: 15 ),
 
-                          Text(article.title,
+                          Text(widget.article.title,
                             style: TextStyle(
                               fontSize: 26,
                             ),
@@ -81,13 +121,13 @@ class ArticlePage extends StatelessWidget {
                             children: <Widget>[
                               ClipRRect(
                                 borderRadius: BorderRadius.circular(6),
-                                child: Image.asset(article.authorImage,
+                                child: Image.asset(widget.article.authorImage,
                                   width: 25,
                                   height: 25,
                                 ),
                               ),
                               SizedBox(width: 10),
-                              Text('${article.author}   |  ${article.time}',
+                              Text('${widget.article.author}   |  ${widget.article.time}',
                                 style: TextStyle(
                                   fontSize: 16
                                 ),
@@ -97,18 +137,31 @@ class ArticlePage extends StatelessWidget {
 
                           SizedBox( height: 20 ),
 
-                          Text(article.body,
+                          Text(widget.article.body,
                             style: TextStyle(
                               color: Colors.black54,
                               fontSize: 18,
                             )
                           ),
 
+                          SizedBox( height: 20 ),
+
+                          ...widget.article.gallery.map((image) {
+                            return Container(
+                              margin: EdgeInsets.symmetric(vertical: 5),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(8)
+                              ),
+                              child: Image.asset(image),
+                            );
+                          }).toList(),
+
                         ],
                       ),
                     ),
 
-                    ...article.gallery,
+                    
                   ],
                 ),
               ),
@@ -121,15 +174,29 @@ class ArticlePage extends StatelessWidget {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      IconButton(
-                          padding: EdgeInsets.all(16),
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: topReached ? Colors.pink.withOpacity(0.5) : Colors.transparent
+                        ),
+                        child: IconButton(
+                          padding: EdgeInsets.all(12),
                           icon: backIcon,
                           onPressed: () => Navigator.pop(context),
+                        )
                       ),
-                      IconButton(
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: topReached ? Colors.pink.withOpacity(0.5) : Colors.transparent
+                        ),
+                        child: IconButton(
                           padding: EdgeInsets.all(16),
-                          icon: saveIcon,
-                          onPressed: () {},
+                          icon: isSaved
+                            ? savedIcon
+                            : saveIcon,
+                          onPressed: () => setState(() => isSaved = !isSaved),
+                        )
                       )
                     ],
                   ),
@@ -137,8 +204,8 @@ class ArticlePage extends StatelessWidget {
               ),
             ],
           ),
-        
-      
+        )
+      )
     );
   }
 }
